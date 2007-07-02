7f96d127bdf59ce06c06a50785ebfac327fdf2a5
Phil Christensen <phil@bubblehouse.org>
2007-07-02 21:06:18 +0000

    Beginning to flesh out ideas for cms components
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@322 ef98d105-df06-0410-9ba4-9cf3429d62c9

---
+++ examples/sabi/modu/plugins/sabi_default_site.py
@@ -0,0 +1,30 @@
+# modu
+# Copyright (C) 2007 Phil Christensen
+#
+# $Id$
+#
+# See LICENSE for details
+
+from modu.web import resource
+from modu.web.app import ISite
+
+from zope.interface import classProvides
+from twisted import plugin
+
+import os
+
+class SabiDefaultSite(object):
+	classProvides(plugin.IPlugin, ISite)
+	
+	def configure_app(self, app):
+		app.base_domain = 'localhost:8888'
+		app.base_path = '/modu/examples/sabi'
+		self.load_plugins(app)
+	
+	def load_plugins(self, app):
+		# import modu.sabi.plugins
+		# reload(modu.sabi.plugins)
+		# 
+		# for site_plugin in plugin.getPlugins(plugin.ISabiPlugin, modu.sabi.plugins):
+		# 	app.activate(site_plugin(app))
+		pass
