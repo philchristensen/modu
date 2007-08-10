57fa7a1d1313f10a66ba84cedd42886923f43987
Phil Christensen <phil@bubblehouse.org>
2007-08-10 03:48:03 +0000

    fleshing out itemdef
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@416 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- modu/web/editable.py
+++ modu/web/editable.py
@@ -16,22 +16,69 @@ class IEditable(storable.IStorable):
 		Return an object/datastructure representing the
 		fields and behaviors for this object's editable
 		forms.
+		
+		Maybe this should be a class variable instead?
+		If it were, we could use something cool like
+		descriptors to implement magic.
 		"""
 
 class itemdef(dict):
-	def __init__(self, **kwargs):
-		for k, v in kwargs.iteritems():
-			if not(isinstance(v, definition)):
-				raise ValueError("'%s' is not a valid definition." % k)
-			
+	def __init__(self, __config=definition(), **fields):
+		for name, field in fields.iteritems():
+			if not(isinstance(field, definition)):
+				raise ValueError("'%s' is not a valid definition." % name)
+			field.name = name
+		
 		self.update(kwargs)
+	
+	def get_detail_form(self, storable):
+		"""
+		Return a FormNode that represents this item
+		"""
+		frm = form.FormNode('%s-form' % storable.get_table())
+		for name, field in self.iteritems():
+			frm.children[name] = field.get_detail_element(storable)
+		# set special array stuff, like pre and postwrite (as callbacks)
+		return frm
+	
+	def get_list_form(self, storable):
+		"""
+		Return a FormNode that represents this item
+		"""
+		frm = form.FormNode(form_name)
+		for name, field in self.iteritems():
+			frm.children[name] = field.get_list_element(storable)
+		# set special array stuff, like pre and postwrite (as callbacks)
+		# set frm.theme to a special list-drawing theme
+		return frm
 
 class definition(dict):
-	def __init__(self, **kwargs):
-		self.update(kwargs)
+	def __init__(self, **params):
+		self.name = None
+		self.update(params)
+	
+	def get_list_element(self, storable):
+		"""
+		Return a FormNode that represents this field in the resulting form.
+		"""
+		attribs = {}
+		# iterate through definition to define form element
+		# load storable content
+		frm = form.FormNode(self.name)(**attribs)
+		return frm
+	
+	def get_detail_element(self, storable):
+		"""
+		Return a FormNode that represents this field in the resulting form.
+		"""
+		attribs = {}
+		# iterate through definition to define form element
+		# load storable content
+		frm = form.FormNode(self.name)(**attribs)
+		return frm
 
 sample_itemdef = itemdef(
-	__special		= definition(
+	__config		= definition(
 						postwrite_callback			= 'user_postwrite',
 						prewrite_callback			= 'user_prewrite'
 					),
