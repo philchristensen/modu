cf41e0e2a4097f778c89d8313700581c6b4e73ff
Phil Christensen <phil@bubblehouse.org>
2007-12-13 20:39:12 +0000

    Small tweaks to jquery AC
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@628 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- modu/editable/datatypes/relational.py
+++ modu/editable/datatypes/relational.py
@@ -9,6 +9,8 @@
 Datatypes to manage foreign key relationships.
 """
 
+import time
+
 from zope.interface import implements
 
 from modu.editable import IDatatype, define
@@ -116,7 +118,7 @@ class ForeignAutocompleteField(define.definition):
 		ac_cb_id = '%s-%s-ac-callback' % (form_name, self.name)
 		ac_url = req.get_path(req.prepath, 'autocomplete', storable.get_table(), self.name)
 		
-		prefs = 'autoFill:1, selectFirst:1, selectOnly:1, minChars:%d' % self.get('min_chars', 3)
+		prefs = 'autoFill:1, selectFirst:1, matchSubset:0, selectOnly:1, extraParams:{t:%d}, minChars:%d' % (int(time.time()), self.get('min_chars', 3))
 		ac_javascript = '$("#%s").autocomplete("%s", '
 		ac_javascript += '{onItemSelect:select_foreign_item("%s"), %s});'
 		ac_javascript = ac_javascript % (ac_id, ac_url, ac_cb_id, prefs)
@@ -313,7 +315,7 @@ class ForeignMultipleAutocompleteField(ForeignMultipleSelectField):
 		form_name = '%s-form' % storable.get_table()
 		ac_id = '%s-%s-autocomplete' % (form_name, self.name)
 		select_id = '%s-foreign-select' % self.name
-		ac_url = req.get_path(req.prepath, 'autocomplete', storable.get_table(), self.name)
+		ac_url = req.get_path(req.prepath, 'autocomplete', storable.get_table(), self.name) + '?time=' + str(time.time())
 		
 		hidden_options = ''
 		for value in options:
@@ -323,7 +325,7 @@ class ForeignMultipleAutocompleteField(ForeignMultipleSelectField):
 		select_frm(type='select', options=options, size=self.get('size', 5),
 					multiple=None, suffix=hidden_options + '<br/>', attributes={'id':select_id})
 		
-		prefs = 'autoFill:1, selectFirst:1, selectOnly:1, minChars:%d' % self.get('min_chars', 3)
+		prefs = 'autoFill:1, selectFirst:1, matchSubset:0, selectOnly:1, extraParams:{t:%d}, minChars:%d' % (int(time.time()), self.get('min_chars', 3))
 		ac_js = '$("#%s").autocomplete("%s", {onItemSelect:add_foreign_item("%s", "%s"), %s});' % (ac_id, ac_url, form_name, self.name, prefs)
 		ac_controls = tags.script(type='text/javascript')[ac_js]
 		
