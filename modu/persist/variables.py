ca6b4b0ac9224cfa2326497846415c5299a4f9e7
Phil Christensen <phil@bubblehouse.org>
2008-04-15 20:09:55 +0000

    added persistent variables feature, added table to schema
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@833 ef98d105-df06-0410-9ba4-9cf3429d62c9

---
+++ modu/persist/variables.py
@@ -0,0 +1,46 @@
+# modu
+# Copyright (C) 2007-2008 Phil Christensen
+#
+# $Id: sql.py 764 2008-02-12 19:45:58Z phil $
+#
+# See LICENSE for details
+
+"""
+Persistent variables.
+
+Somewhat modeled after Drupal variable system, this is a way for modu
+developers to easily save some persistent variables.
+"""
+
+try:
+	import cPickle as pickle
+except ImportError, e:
+	import pickle
+
+from modu.persist import storable
+
+def get(req, name, default=None):
+	result = _get_variable(req, name)
+	if(result is None):
+		return default
+	return pickle.loads(result.value)
+
+def set(req, name, value):
+	result = _get_variable(req, name)
+	if(result is None):
+		result = storable.Storable('variable')
+		result.name = name
+	result.value = pickle.dumps(value, 1)
+	req.store.save(result)
+
+def delete(req, name):
+	result = _get_variable(req, name)
+	if(result is not None):
+		req.store.destroy(result)
+
+def _get_variable(req, variable_name):
+	req.store.ensure_factory('variable')
+	result = req.store.load_one('variable', name=variable_name)
+	if(result and hasattr(result.value, 'tostring')):
+		result.value = result.value.tostring()
+	return result
