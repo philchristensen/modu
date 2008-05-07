9fb56e308eca26a36143edc3b5bd60f0df2aaeb9
Phil Christensen <phil@bubblehouse.org>
2008-05-07 16:39:35 +0000

    added faq model
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@852 ef98d105-df06-0410-9ba4-9cf3429d62c9

---
+++ examples/modusite/modusite/model/faq.py
@@ -0,0 +1,16 @@
+# modusite
+# Copyright (C) 2008 Phil Christensen
+#
+# $Id$
+#
+
+from modu.persist import storable
+
+class FAQ(storable.Storable):
+	def __init__(self):
+		super(FAQ, self).__init__('faq')
+	
+	def get_answerer(self):
+		store = self.get_store()
+		user = store.load_one('user', id=self.answered_by)
+		return user.username
