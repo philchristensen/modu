1794b95d4b57cda58ecea93064a5ce560c12c442
Phil Christensen <phil@bubblehouse.org>
2008-05-28 18:01:50 +0000

    added jquery 1.2.6, some modu.assets revisions
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@890 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- modu/assets/__init__.py
+++ modu/assets/__init__.py
@@ -5,33 +5,33 @@
 #
 # See LICENSE for details
 
-JQUERY_VERSION = '1.2.4'
-JQUERY_UI_VERSION = '1.0'
+JQUERY_VERSION = '1.2.6'
+JQUERY_UI_VERSION = '1.5b4'
 JQUERY_PACKED = True
 JQUERY_UI_PACKED = True
 
-def get_jquery_path(req):
-	if(JQUERY_PACKED):
+def get_jquery_path(req, packed=True):
+	if(packed):
 		packed = '.pack'
 	else:
 		packed = ''
 	return req.get_path('/assets/jquery/jquery-%s%s.js' % (JQUERY_VERSION, packed))
 
-def get_jquery_ui_path(req, full=False):
+def get_jquery_ui_path(req, full=False, packed=True):
 	if(full):
 		core = 'jquery.ui-all-%s' % JQUERY_UI_VERSION
 	else:
 		core = 'ui.core'
 	
-	if(JQUERY_UI_PACKED and JQUERY_UI_VERSION != '1.0'):
+	if(packed and JQUERY_UI_VERSION != '1.0'):
 		url = req.get_path('/assets/jquery/jquery.ui-%s' % JQUERY_UI_VERSION, 'packed-javascript', core + '.packed.js')
 	else:
 		url = req.get_path('/assets/jquery/jquery.ui-%s' % JQUERY_UI_VERSION, '%s.js' % core)
 	
 	return url
 
-def get_jquery_ui_component_path(req, module):
-	if(JQUERY_UI_PACKED and JQUERY_UI_VERSION != '1.0'):
+def get_jquery_ui_component_path(req, module, packed=True):
+	if(packed and JQUERY_UI_VERSION != '1.0'):
 		url = req.get_path('/assets/jquery/jquery.ui-%s' % JQUERY_UI_VERSION, 'packed-javascript/%s.packed.js' % module)
 	else:
 		url = req.get_path('/assets/jquery/jquery.ui-%s' % JQUERY_UI_VERSION, '%s.js' % module)
