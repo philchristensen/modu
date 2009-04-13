297bcc857c9ab928dea5be0d45961ed225ad776b
Phil Christensen <phil@bubblehouse.org>
2009-04-13 19:42:21 +0000

    removed dropin.cache
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1035 ef98d105-df06-0410-9ba4-9cf3429d62c9

---
+++ __testsite__.py
@@ -0,0 +1,135 @@
+# modu
+# Copyright (C) 2007-2008 Phil Christensen
+#
+# $Id$
+#
+# See LICENSE for details
+
+"""
+ISite implementors and sample resource classes used in the testing framework.
+
+Since the ISite implementors don't actually use real (or even valid)
+hostnames, none of these sites should ever activate in normal use.
+"""
+
+from twisted import plugin
+
+from zope.interface import classProvides, implements
+
+from modu.web import app, resource
+
+class TestResource(resource.Resource):
+	"""
+	This simple resource returns plaintext containing
+	the postpath used in the request.
+	"""
+	implements(resource.IContent)
+	
+	def prepare_content(self, req):
+		"""
+		@see: L{modu.web.app.resource.IContent.prepare_content()}
+		"""
+		if(req.postpath):
+			if(req.postpath[0] == 'exception'):
+				raise Exception('requested exception')
+			
+	
+	def get_content_type(self, req):
+		"""
+		@see: L{modu.web.app.resource.IContent.get_content_type()}
+		"""
+		return 'text/plain'
+	
+	def get_content(self, req):
+		"""
+		@see: L{modu.web.app.resource.IContent.get_content()}
+		"""
+		return '/'.join(req.postpath)
+
+
+class TestDelegateResource(resource.Resource):
+	"""
+	This resource delegate will return an instance of
+	TestResource.
+	"""
+	implements(resource.IResourceDelegate)
+	
+	def get_delegate(self, req):
+		"""
+		@see: L{modu.web.app.resource.IResourceDelegate.get_delegate()}
+		"""
+		return TestResource()
+
+
+
+class TestAccessControlResource(resource.Resource):
+	"""
+	This resource will always return a 401 Unauthorized when
+	requested.
+	"""
+	implements(resource.IAccessControl)
+	
+	def check_access(self, req):
+		"""
+		@see: L{modu.web.app.resource.IAccessControl.check_access()}
+		"""
+		app.raise401(req.path)
+
+
+class BasicTestSite(object):
+	"""
+	This site activates only the most basic features of modu, running at /app-test.
+	"""
+	classProvides(plugin.IPlugin, app.ISite)
+	
+	def initialize(self, application):
+		"""
+		@see: L{modu.app.ISite.initialize()}
+		"""
+		application.base_domain = '____basic-test-domain____:1234567'
+		application.base_path = '/app-test'
+		application.db_url = None
+		application.session_class = None
+		application.initialize_store = False
+		application.activate('/test-resource', TestResource)
+		application.activate('/test-delegate', TestDelegateResource)
+		application.activate('/test-access', TestAccessControlResource)
+
+
+class BasicRootTestSite(object):
+	"""
+	This site activates only the most basic features of modu, running at the site root.
+	"""
+	classProvides(plugin.IPlugin, app.ISite)
+	
+	def initialize(self, application):
+		"""
+		@see: L{modu.app.ISite.initialize()}
+		"""
+		application.base_domain = '____basic-test-domain____:1234567'
+		application.db_url = None
+		application.session_class = None
+		application.initialize_store = False
+		application.activate('/test-resource', TestResource)
+		application.activate('/test-delegate', TestDelegateResource)
+		application.activate('/test-access', TestAccessControlResource)
+
+
+class StoreTestSite(object):
+	"""
+	This site activates a database connection and a Store.
+	"""
+	classProvides(plugin.IPlugin, app.ISite)
+	
+	def initialize(self, application):
+		"""
+		@see: L{modu.app.ISite.initialize()}
+		"""
+		application.base_domain = '____store-test-domain____:1234567'
+		application.base_path = '/app-test'
+		application.session_class = None
+		application.activate('/test-resource', TestResource)
+		application.activate('/test-delegate', TestDelegateResource)
+		application.activate('/test-access', TestAccessControlResource)
+
+
