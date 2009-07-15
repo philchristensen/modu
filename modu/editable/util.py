70da4051364a1d63722d5cfb08e19db8450b78cf
Phil Christensen <phil@bubblehouse.org>
2009-07-15 17:51:00 +0000

    added missing support function for url code generator
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1083 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- modu/editable/util.py
+++ modu/editable/util.py
@@ -5,6 +5,10 @@
 #
 # See LICENSE for details
 
+import re
+
+from modu.persist import sql
+
 def get_url_code_callback(source_field, destination_field):
 	"""
 	Auto-generate URL codes and give opportunity for approval.
@@ -29,7 +33,7 @@ def get_url_code_callback(source_field, destination_field):
 		pool = storable.get_store().pool
 		
 		try:
-			verified_urlcode = model.create_url_code(phrase, storable.get_table(), req.store.pool,
+			verified_urlcode = create_url_code(phrase, storable.get_table(), req.store.pool,
 				check_field='url_code', user_defined=existing_url_code, origin_id=storable.get_id())
 		except ValueError, e:
 			form[destination_field](value=str(e))
@@ -41,3 +45,52 @@ def get_url_code_callback(source_field, destination_field):
 		
 		return True
 	return _generator
+
+def create_url_code(phrase, table, pool, check_field='url_code', user_defined=None, origin_id=None):
+	def _make_code(text):
+		urlcode = re.sub(r'\W+', '-', text.lower())
+		return re.sub(r'^\-*(.*?)\-*$', lambda(m): m.group(1), urlcode)
+	
+	# Create a default url code
+	urlcode = _make_code(phrase)
+	
+	# If the user has supplied a code...
+	if(user_defined):
+		# And it isn't a valid url code
+		if(_make_code(user_defined) != user_defined):
+			# we've already failed once
+			attempt = 2
+		# Otherwise...
+		else:
+			# Check the given code
+			attempt = 1
+			urlcode = user_defined
+	# Otherwise, stay as is
+	else:
+		attempt = 1
+	
+	def _check_code(urlcode):
+		if not(origin_id):
+			attribs = {
+				check_field:			urlcode
+			}
+		else:
+			attribs = {
+				'id':					sql.NOT(origin_id),
+				check_field:			urlcode
+			}
+		return pool.runQuery(sql.build_select(table, attribs))
+	
+	verified_urlcode = urlcode
+	
+	results = _check_code(verified_urlcode)
+	while(results):
+		attempt += 1
+		verified_urlcode = '%s-%d' % (urlcode, attempt)
+		results = _check_code(verified_urlcode)
+	
+	if(user_defined is not None and attempt > 1 and user_defined != verified_urlcode):
+		raise ValueError(verified_urlcode)
+	
+	return verified_urlcode
+
