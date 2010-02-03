1b9c2aabe08682a9932214d31350122c6cc9f947
Phil Christensen <phil@bubblehouse.org>
2010-02-03 18:52:47 +0000

    moved modu to src
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1226 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ from setuptools import setup, find_packages
 setup(
 	name			= "modu",
 	version 		= "1.0",
-	packages		= find_packages(),
+	packages		= find_packages('src'),
+	package_dir		= {'':'src'}
 	scripts 		= ['bin/mkmodu.py'],
 	
 	install_requires 		= ['twisted'],
