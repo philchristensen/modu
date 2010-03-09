942dd4ed9606879f33bd714e0d71ef2af4e5add3
Phil Christensen <phil@bubblehouse.org>
2010-03-09 22:15:45 +0000

    added acl check to postwrite fields
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1313 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- src/modu/editable/define.py
+++ src/modu/editable/define.py
@@ -518,7 +518,11 @@ class itemdef(OrderedDict):
 		
 		postwrite_fields = {}
 		for name, definition in self.iteritems():
-			if not definition.get('implicit_save', True) or definition.get('read_only', False):
+			if(not definition.get('implicit_save', True)):
+				continue
+			elif(definition.get('read_only', False)):
+				continue
+			elif(not definition.allows(req.user)):
 				continue
 			elif(definition.is_postwrite_field()):
 				postwrite_fields[name] = definition
