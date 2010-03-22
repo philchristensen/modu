499a6ac4d8536ba609c19d33f7ce62b7f1a8368e
Phil Christensen <phil@bubblehouse.org>
2010-03-22 17:14:33 +0000

    added item title field to standard editables
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1320 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- src/modu/editable/datatypes/relational.py
+++ src/modu/editable/datatypes/relational.py
@@ -59,6 +59,72 @@ class ForeignLabelField(define.definition):
 		
 		return frm
 
+class ItemTitleField(ForeignLabelField):
+	"""
+	Display the item title for this record.
+	
+	Given the item_id and item_table fields available in some records,
+	this field will display the proper title.
+	"""
+	def get_element(self, req, style, storable):
+		"""
+		@see: L{modu.editable.define.definition.get_element()}
+		"""
+		store = storable.get_store()
+		
+		label = None
+		value = None
+		
+		label_col = self.get('flabel', 'title')
+		value_col = 'id'
+		
+		table = getattr(storable, 'item_table', None)
+		if not(table):
+			table = self.get('ftable')
+		
+		item_value = getattr(storable, self.get_column_name(), None)
+		
+		if(table is None or item_value is None):
+			results = None
+		else:
+			# We select * in case the particular item doesn't have a title field
+			foreign_label_query = "SELECT * FROM %s WHERE %s = %%s" % (table, value_col)
+			foreign_label_query = sql.interp(foreign_label_query, [item_value])
+			
+			results = store.pool.runQuery(foreign_label_query)
+			if(results):
+				value = results[0][value_col]
+				label = results[0].get(label_col, '(label not found)')
+		
+		frm = form.FormNode(self.name)
+		suffix = ''
+		prefix = ''
+		if(style == 'listing'):
+			frm(type='hidden', value=value)
+			if(table and value):
+				label = tags.a(href=req.get_path(req.prepath, 'detail', table, value))[label]
+			frm(type='label', value=label)
+		else:
+			if not(label):
+				label = '(no link available)'
+			
+			frm(type='hidden', value=value)
+			if(table and value):
+				prefix = tags.a(href=req.get_path(req.prepath, 'detail', table, value))[label]
+			else:
+				prefix = label
+		
+		frm(prefix=prefix, suffix=suffix)
+		
+		return frm
+	
+	def update_storable(self, req, form, storable):
+		"""
+		No operation.
+		
+		@see: L{modu.editable.define.definition.update_storable()}
+		"""
+		pass
 
 class ForeignSelectField(define.definition):
 	"""
@@ -106,7 +172,6 @@ class ForeignSelectField(define.definition):
 			frm(type='select', value=getattr(storable, self.get_column_name(), None), options=options)
 
 		return frm
-
 
 class ForeignAutocompleteField(define.definition):
 	"""
