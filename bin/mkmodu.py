4e0cd8989a0252f5fd11ec21d2d2e61a59bf42fd
Phil Christensen <phil@bubblehouse.org>
2010-04-21 17:59:47 +0000

    refactored mkmodu in preparation for mkmodel and mkitemdef scripts
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1335 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- bin/mkmodu.py
+++ bin/mkmodu.py
@@ -18,12 +18,14 @@ Python search path, either globally, or by a web container-specific
 directive.
 """
 
-import os, os.path, sys, time
-import pkg_resources as pkg
+import os.path, sys
+
+import warnings
+warnings.simplefilter('ignore', UserWarning)
 
 from twisted.python import usage
 
-from Cheetah.Template import Template
+from modu import skel
 
 class Options(usage.Options):
 	"""
@@ -53,60 +55,13 @@ if(__name__ == '__main__'):
 	
 	destroot = os.path.abspath(os.path.normpath(config['shortname']))
 	
-	config['shortname'] = os.path.basename(destroot)
-	
+	shortname = config['shortname']
 	if not config['longname']:
 		config['longname'] = config['shortname']
 	if not config['author']:
 		config['author'] = config['shortname']
+	del config['shortname']
 	
-	def copy_skel(path, dest):
-		#import pdb
-		for filename in pkg.resource_listdir('modu', path):
-			resource_path = os.path.join(path, filename)
-			
-			if(pkg.resource_isdir('modu', resource_path)):
-				directory = filename
-				if(directory.startswith('.')):
-					continue
-				if(directory.startswith('project')):
-					directory = directory.replace('project', config['shortname'])
-				
-				new_dest = os.path.join(dest, directory)
-				
-				if not(os.path.isdir(new_dest)):
-					os.makedirs(new_dest)
-				#pdb.set_trace()
-				copy_skel(resource_path, new_dest)
-			else:
-				if(filename.startswith('.')):
-					continue
-				
-				with pkg.resource_stream('modu', resource_path) as file_stream:
-					if(filename.endswith('.tmpl')):
-						template_class = Template.compile(file=file_stream)
-						
-						variables = dict(
-							project_name = config['shortname'],
-							project_description = config.get('longname', config['shortname']),
-							copyright_holder = config.get('author', config['shortname']),
-							year = time.strftime('%Y')
-						)
-						
-						output = str(template_class(searchList=[variables]))
-						new_filename = filename[:-5]
-						if(new_filename.startswith('project')):
-							new_filename = new_filename.replace('project', config['shortname'])
-						new_path = os.path.join(dest, new_filename)
-						
-						#pdb.set_trace()
-						with open(new_path, 'w') as new_file:
-							new_file.write(output)
-					else:
-						# copy the file
-						output_filename = os.path.join(dest, filename)
-						#pdb.set_trace()
-						with open(output_filename, 'w') as output_file:
-							shutil.copyfileobj(file_stream, output_file)
+	skel.create_project(shortname, destroot, **config)
 	
-	copy_skel('skel', destroot)
+	print "Created skeleton `%s` project in %s" % (shortname, destroot) 
---
+++ src/modu/skel/__init__.py
@@ -0,0 +1,77 @@
+# modu
+# Copyright (c) 2006-2010 Phil Christensen
+# http://modu.bubblehouse.org
+#
+# $Id$
+#
+# See LICENSE for details
+
+"""
+Utilities to create stub files for a modu project.
+"""
+
+import os, os.path, time
+
+import pkg_resources as pkg
+
+from Cheetah.Template import Template
+
+def create_project(shortname, dest, path='skel', **config):
+	for filename in pkg.resource_listdir('modu', path):
+		if(filename.startswith('__init__.py')):
+			continue
+		
+		resource_path = os.path.join(path, filename)
+		
+		if(pkg.resource_isdir('modu', resource_path)):
+			directory = filename
+			if(directory.startswith('.')):
+				continue
+			if(directory.startswith('project')):
+				directory = directory.replace('project', shortname)
+			
+			new_dest = os.path.join(dest, directory)
+			
+			if not(os.path.isdir(new_dest)):
+				os.makedirs(new_dest)
+			create_project(shortname, new_dest, resource_path, **config)
+		else:
+			if(filename.startswith('.')):
+				continue
+			
+			new_file = None
+			output_file = None
+			file_stream = pkg.resource_stream('modu', resource_path)
+			try:
+				if(filename.endswith('.tmpl')):
+					template_class = Template.compile(file=file_stream)
+					
+					variables = dict(
+						project_name = shortname,
+						project_description = config.get('longname', shortname),
+						copyright_holder = config.get('author', shortname),
+						year = time.strftime('%Y')
+					)
+					
+					output = str(template_class(searchList=[variables]))
+					new_filename = filename[:-5]
+					if(new_filename.startswith('project')):
+						new_filename = new_filename.replace('project', shortname)
+					new_path = os.path.join(dest, new_filename)
+					
+					new_file = open(new_path, 'w')
+					new_file.write(output)
+					new_file.close()
+				else:
+					# copy the file
+					output_filename = os.path.join(dest, filename)
+					output_file = open(output_filename, 'w')
+					shutil.copyfileobj(file_stream, output_file)
+					output_file.close()
+			finally:
+				if(new_file and not new_file.closed):
+					new_file.close()
+				if(output_file and not output_file.closed):
+					output_file.close()
+				file_stream.close()
+
