ca21becaf359a4a2c1903bc351a0a78e37e1afc9
Phil Christensen <phil@bubblehouse.org>
2010-08-06 19:40:57 +0000

    added traceback logging to wsgi hook
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1359 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- src/modu/web/modwsgi.py
+++ src/modu/web/modwsgi.py
@@ -1,4 +1,8 @@
 from modu.web import app
 
 def application(environ, start_response):
-	return app.handler(environ, start_response)
+	try:
+		return app.handler(environ, start_response)
+	except:
+		import traceback
+		traceback.print_exc(None, environ['wsgi.errors'])
