c3b3c5c640450720840dca072bda8aa7f27e40b7
Phil Christensen <phil@bubblehouse.org>
2010-08-10 19:51:27 +0000

    removed exception handling from modwsgi script
    git-svn-id: http://svn.bubblehouse.org/modu/trunk@1360 ef98d105-df06-0410-9ba4-9cf3429d62c9

--- src/modu/web/modwsgi.py
+++ src/modu/web/modwsgi.py
@@ -1,8 +1,4 @@
 from modu.web import app
 
 def application(environ, start_response):
-	try:
-		return app.handler(environ, start_response)
-	except:
-		import traceback
-		traceback.print_exc(None, environ['wsgi.errors'])
+	return app.handler(environ, start_response)
