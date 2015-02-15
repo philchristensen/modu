1848f7a9a8a4cba76324cf6b6032ea027a389c39
Phil Christensen <pchristensen@freelancersunion.org>
2015-02-15 14:05:15 -0500

    revert to old version of jquery

--- src/modu/assets/__init__.py
+++ src/modu/assets/__init__.py
@@ -7,11 +7,13 @@
 
 from modu.util import tags
 
-DEFAULT_JQUERY_VERSION = '1.11.0'
-DEFAULT_JQUERY_UI_VERSION = '1.9.2'
+# DEFAULT_JQUERY_VERSION = '1.11.0'
+# DEFAULT_JQUERY_UI_VERSION = '1.9.2'
+DEFAULT_JQUERY_VERSION = '1.4.2'
+DEFAULT_JQUERY_UI_VERSION = '1.7.1'
 
 def activate_jquery(req):
-	req.content.report('header', tags.script(type="text/javascript", src="//ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js" % DEFAULT_JQUERY_VERSION)[''])
+	req.content.report('header', tags.script(type="text/javascript", src="//code.jquery.com/jquery-%s.min.js" % DEFAULT_JQUERY_VERSION)[''])
 
 def activate_jquery_ui(req):
-	req.content.report('header', tags.script(type="text/javascript", src="//ajax.googleapis.com/ajax/libs/jqueryui/%s/jquery-ui.min.js" % DEFAULT_JQUERY_UI_VERSION)[''])
+	req.content.report('header', tags.script(type="text/javascript", src="//code.jquery.com/ui/%s/jquery-ui.min.js" % DEFAULT_JQUERY_UI_VERSION)[''])
